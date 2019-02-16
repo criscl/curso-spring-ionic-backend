bf1e1b5c91c5cd5f5044027b892522123a279c7b
Cristiano CL <cerqueiralopes@hotmail.com>
2019-02-16 01:07:22 -0200

    Começando a criar as camadas repository e service[B

--- src/main/java/com/cristiano/cursospring/CursospringApplication.java
+++ src/main/java/com/cristiano/cursospring/CursospringApplication.java
@@ -1,14 +1,35 @@
 package com.cristiano.cursospring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursospringApplication {
+import com.cristiano.cursospring.domain.Categoria;
+import com.cristiano.cursospring.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursospringApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		  Categoria cat1 = new Categoria(1, "Informática"); 
+		  Categoria cat2 = new Categoria(2, "Escritório"); 
+		  
+		  categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		 		
+	}
+
+	
 }
 
