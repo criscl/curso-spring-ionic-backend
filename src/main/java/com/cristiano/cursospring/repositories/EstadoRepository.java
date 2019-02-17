7df5769df13d7bbea4c76978d92358dd3986ebcc
Cristiano CL <cerqueiralopes@hotmail.com>
2019-02-16 22:02:52 -0200

    Cidade e estado

---
+++ src/main/java/com/cristiano/cursospring/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.cristiano.cursospring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cristiano.cursospring.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository <Estado, Integer> {
+
+}
